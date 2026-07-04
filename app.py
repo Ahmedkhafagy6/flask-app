28f2713fae78e52f7868d01cbc94dd436e542656
ahmed <ahmedmk6696@gmail.com>
2026-07-04 18:21:17 +0000

    v1

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Deployed by Ahmed - v1.0"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
