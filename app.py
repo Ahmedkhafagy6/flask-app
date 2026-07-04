2e4ae3b1f916804feb3cda5b8d40d663c6c554b8
ahmed <ahmedmk6696@gmail.com>
2026-07-04 18:22:04 +0000

    V2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deployed by Ahmed - v1.0"
+    return "Deployed by Ahmed - v2.0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
