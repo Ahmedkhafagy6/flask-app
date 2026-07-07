50c2d7f9f5dadb6524658a9329c9be6b2e6dd609
ahmed <ahmedmk6696@gmail.com>
2026-07-07 20:01:44 +0000

    app update v4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deployed by Ahmed Khafagy - Full CI/CD V3.0"
+    return "Deployed by Ahmed Khafagy - v4.0 - Kubernetes rolling update"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
